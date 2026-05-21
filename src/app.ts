e010dd7293cbb20c6fa519151545979cc274cfa5
Fardin Ahmed <fardin.cse25@gmail.com>
2026-05-21 11:55:19 +0600

    server initialized

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express, { type Application, type Request, type Response } from "express"
+// import { userRoute } from "./modules/user/user.route"
+// import { profileRoute } from "./modules/profile/profile.route";
+// import { authRoute } from "./modules/auth/auth.route";
+import CookieParser from "cookie-parser"
+import cors from "cors"
+import config from "./config";
+import globalErrorHandler from "./middlewares/globalErrorHandler";
+
+
+const app: Application = express()
+app.use(express.json());
+app.use(CookieParser());
+app.use(cors({ origin: `http://localhost:${config.port}` }))
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "DevPulse Server",
+    author: "Fardin Ahmed",
+  });
+});
+
+// app.use("/api/users", userRoute);
+// app.use("/api/profiles", profileRoute);
+// app.use("/api/auth", authRoute)
+
+app.use(globalErrorHandler)
+
+export default app;
---
+++ src/modules/user/user.route.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+// import { userController } from "./user.controller"
+// import auth from "../../middlewares/auth";
+// import { User_Role } from "../../types";
+
+const router = Router();
+
+
+//! Create User
+// router.post("/", userController.createUser);
+
+// //! Get All Users
+// router.get("/", auth(User_Role.admin, User_Role.agent), userController.getAllUsers);
+
+// //! Get Single User
+// router.get("/:id", userController.getSingleUser);
+
+// //! Update User
+// router.put("/:id", userController.updateUser);
+
+// //! Delete User
+// router.delete("/:id", userController.deleteUser);
+
+
+export const userRoute = router;
